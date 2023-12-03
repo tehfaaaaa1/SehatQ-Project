cd5b9f02cc165915638df8fd0d4d3c22f3407c8f
fatee <fateehganteng@gmail.com>
2023-12-03 18:34:59 +0700

    Hellow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.projectsehatq"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.projectsehatq"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
