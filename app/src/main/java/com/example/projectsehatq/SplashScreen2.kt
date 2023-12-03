31bcda7c7cce0a65666acef583d72b8318133c15
fatee <fateehganteng@gmail.com>
2023-12-03 20:40:40 +0700

    Hellow

--- app/src/main/java/com/example/projectsehatq/SplashScreen2.kt
+++ app/src/main/java/com/example/projectsehatq/SplashScreen2.kt
@@ -5,24 +5,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.TextView
 
-class SplashScreen1 : AppCompatActivity() {
+class SplashScreen2 : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen1)
+        setContentView(R.layout.activity_splash_screen2)
 
-        val btnNext:ImageButton = findViewById(R.id.btnNext)
-        val tvNext:TextView = findViewById(R.id.tvNext)
+        val btnNext: ImageButton = findViewById(R.id.btnNext)
+        val tvNext: TextView = findViewById(R.id.tvNext)
+        val intent = Intent(this, SplashScreen3::class.java)
 
         btnNext.setOnClickListener {
-            val intent = Intent(this, SplashScreen2::class.java)
             startActivity(intent)
         }
 
         tvNext.setOnClickListener {
-            val intent = Intent(this, SplashScreen2::class.java)
             startActivity(intent)
         }
     }
